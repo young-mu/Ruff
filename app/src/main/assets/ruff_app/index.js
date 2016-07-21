7bc994c6e2fd023099cd1f73d744499a5b61eb9c
Young <young.mu@aliyun.com>
2016-07-21 20:04:40 +0800

    Update ruff app

--- app/src/main/assets/ruff_app/index.js
+++ app/src/main/assets/ruff_app/index.js
@@ -1,6 +1,6 @@
 var android = require('./android.js').android;
 
-/*
+
 android.loadUrl('web_app/index.html');
 
 android.on('button1', function() {
@@ -14,13 +14,11 @@ android.on('button2', function() {
 android.on('button3', function() {
     console.log('Hey, I\'m button3 callback');
 });
-*/
 
-android.openPic('ruff_mm/Pictures/test1.jpg');
-/*
-android.openPic('ruff_mm/Pictures/test2.jpg');
 
-android.playAudio('ruff_mm/Audios/test.mp3');
+//android.loadUrl('web_app/index2.html');
 
-android.playVideo('ruff_mm/Videos/test.mp4');
-*/
+//android.openPic('ruff_mm/Pictures/test1.jpg');
+//android.openPic('ruff_mm/Pictures/test2.jpg');
+//android.playAudio('ruff_mm/Audios/test.mp3');
+//android.playVideo('ruff_mm/Videos/test.mp4');

94c0b972768d6c51946251006d3b1ead56cd72cd
Young <young.mu@aliyun.com>
2016-07-27 11:20:16 +0800

    Remove kill process

--- app/src/main/assets/ruff/sdk/ruff_modules/launcher.js
+++ app/src/main/assets/ruff/sdk/ruff_modules/launcher.js
@@ -7,30 +7,7 @@
 var path = require('path');
 
 var tmpPath = '/data/data/com.young.ruff/tmp/';
-
-var PID_FILE_PATH = tmpPath + 'ruff_app.pid';
 var APP_START_FIFO_PATH = tmpPath + 'app_start';
-
-function checkandKillAppInstance() {
-    var mode = parseInt('666', 8);
-    uv.fs_stat(PID_FILE_PATH, function (data, err) { // jshint ignore:line
-        var fd;
-        if (data) {
-            fd = uv.fs_open(PID_FILE_PATH, 'r', mode);
-            var pid = uv.fs_read(fd, 1024, 0).toString();
-            uv.fs_close(fd);
-
-            pid = Number(pid);
-            if (!isNaN(pid)) {
-                uv.kill(pid, 9);
-            }
-        }
-
-        fd = uv.fs_open(PID_FILE_PATH, 'w', mode);
-        uv.fs_write(fd, uv.getpid().toString(), 0);
-        uv.fs_close(fd);
-    });
-}
 
 function launch() {
     var ruffBox = require('ruff');
@@ -54,7 +31,6 @@ function launch() {
     require(entry);
 }
 
-checkandKillAppInstance();
 //trigger uv.run here to refresh timestamp for timers
 uv.run();
 launch();
